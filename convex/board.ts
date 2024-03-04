25dae2e5b7beb30f83bfe09cb3ade904190aef10
Roman Borysenko <roman.borysenko@sysgears.com>
2024-03-04 23:41:28 +0200

    Added room authentication

--- convex/board.ts
+++ convex/board.ts
@@ -1,6 +1,6 @@
 import { v } from 'convex/values';
 import { MAX_CHAR_LENGTH } from '@/constants';
-import { mutation } from './_generated/server';
+import { mutation, query } from './_generated/server';
 
 const images = [
   '/placeholders/1.svg',
@@ -173,6 +173,15 @@ export const unfavorite = mutation({
     }
 
     await ctx.db.delete(existingFavorite._id);
+
+    return board;
+  },
+});
+
+export const get = query({
+  args: { id: v.id('boards') },
+  handler: async (ctx, args) => {
+    const board = ctx.db.get(args.id);
 
     return board;
   },
