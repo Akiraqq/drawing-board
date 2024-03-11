7b4e25fc80f61877b6135c45632c1b0d115cc896
Roman Borysenko <roman.borysenko@sysgears.com>
2024-03-11 17:05:16 +0200

    Created room toolbar

--- app/board/[boardId]/_components/info/info-skeleton.tsx
+++ app/board/[boardId]/_components/info/info-skeleton.tsx
@@ -5,7 +5,7 @@ export const InfoSkeleton: FC = () => {
   return (
     <Skeleton
       className="absolute top-2 left-2 bg-white rounded-md px-1.5
-h-12 flex items-center shadow-md w-[135px]"
+h-12 flex items-center shadow-md w-[300px]"
     />
   );
 };
